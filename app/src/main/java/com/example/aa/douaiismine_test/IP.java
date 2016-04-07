a9ae068b019a1b8b101a6a9900e95b8960a1880a
themp11 <ayoubaboujaafar@yahoo.fr>
2016-04-07 08:55:53 +0200

    Added files via upload

---
+++ app/src/main/java/com/example/aa/douaiismine_test/IP.java
@@ -0,0 +1,14 @@
+package com.example.aa.douaiismine_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.infos_pratiques);
+
+    }
+}
